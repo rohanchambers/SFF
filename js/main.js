74be6ce478dab2b47e372e77d81f81a689f2b6f9
Rohan Chambers <rohanchambers888@yahoo.com>
2014-08-12 18:49:02 +0200

    started layout

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+$(document).ready( function(){
+	//Animate scroll
+	var nav = $('.main-nav li a');
 
+	$(nav).bind('click',function(event){
+		
+		nav.hide();
+	    
+	    var $anchor = $(this);
+	    
+	    $('html, body').stop().animate({
+	   
+	        scrollTop: $($anchor.attr('href')).offset().top
+	   
+	    }, 500, function(){
+		    
+	    	$('.main-nav li a').fadeIn();
+	    		    
+	    });
+
+	    event.preventDefault();
+
+	});
+});
